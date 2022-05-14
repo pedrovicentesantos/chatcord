e1363b91fabbc3fec50c8c34f2e7f432daea5a1c
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-05-14 18:43:42 -0300

    feat: basic chat functionality with socket.io

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+io.on('connection', socket => {
+  socket.emit('message', 'Welcome to the chat app');
+
+  socket.broadcast.emit('message', 'New user joined the chat');
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'User was disconnected');
+  });
+
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg);
+  });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
